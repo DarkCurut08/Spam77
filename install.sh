d387a2abfc7cc803ba2200b98495de8e344f09ee
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2019-10-10 17:16:04 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+pkg update && upgrade
+pkg install php
+pkg install python
+pkg install python2
+pip install requests
+pkg install mechanize
+pip2 install lolcat
+echo " [!] Proses Install Sudah Selesai..." | lolcat
