3053f675dc77057459cfd8955dcf7db9ac36c7e1
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2019-10-10 16:52:42 +0700

    Add files via upload

---
+++ Moreno77.sh
@@ -0,0 +1,27 @@
+clear
+echo "\033[1;32m
+      +---------[Spamer by Moreno77]----------+"
+sleep 1
+echo " 1. Spam SMS\n 2. Spam Call"
+echo " 3. Update Script"
+read -p " [?] Input => " pil;
+case $pil in
+1) cd data
+python SMS.py
+exit
+;;
+2) cd data
+php Call.php
+exit
+;;
+3) echo "[!] Proses Update Script......"
+sleep 2 
+rm -rf Spam77
+git clone https://github.com/DarkCurut08/Spam77
+cd Spam77
+sh Moreno77.sh
+;;
+esac
+done
+done
+
