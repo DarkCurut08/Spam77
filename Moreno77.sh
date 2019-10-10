484f2ee5977e6d3442eb31a2667e0cd3b305fd24
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2019-10-10 19:52:48 +0700

    Update Moreno77.sh

--- Moreno77.sh
+++ Moreno77.sh
@@ -2,7 +2,7 @@ clear
 echo "\n\n\n\033[1;32m      +---------[Spamer by Moreno77]----------+"
 python .py
 sleep 1
-echo "\033[1;32m 1. Spam Grab\n 2. Spam Tokopedia\n 3. Spam OYO
+echo "\033[1;32m 1. Spam Grab\n 2. Spam Tokopedia\n 3. Spam OYO"
 echo " 00. Update Script"
 read -p " [?] Input =>\033[1;33m " pil;
 case $pil in
